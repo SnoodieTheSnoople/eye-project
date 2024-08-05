28e9e3000c90d40299dcfb5013f84679702fc1bc
Cleef <cleefgulfan312@gmail.com>
2024-08-05 16:27:11 +0100

    Page adjustment and header size changed to be larger.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 
-<div class='my-6 mx-4 space-y-4'>
-	<h1 class='flex justify-center items-center font-bold'>Welcome to Smart Readers!</h1>
+<div class='my-10 mx-4 space-y-8'>
+	<h1 class='flex justify-center items-center font-bold text-4xl'>Welcome to Smart Readers!</h1>
 
 	<div class='flex justify-center items-center'>
 		<Button>Get Started</Button>
