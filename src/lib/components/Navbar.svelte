ba1c740529fa90e7d2818cef438b11f6f3ac774b
Cleef <cleefgulfan312@gmail.com>
2024-08-05 16:25:24 +0100

    Navbar styling changes.

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,11 +2,11 @@
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
 
-<Navbar  >
+<Navbar class='bg-primary-800 text-white'>
     <NavBrand href="/">
         <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Smart Readers</span>
     </NavBrand>
-    <NavHamburger  />
+    <NavHamburger/>
     <NavUl >
         <NavLi href="/">Home</NavLi>
     </NavUl>
