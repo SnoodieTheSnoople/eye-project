f981633ddccb23ac8c179e5bf2bb8fc8f3c7ea0d
Cleef <cleefgulfan312@gmail.com>
2024-08-08 17:42:08 +0100

    Setup basic modal component.

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Button, ButtonGroup, Modal } from "flowbite-svelte";
+    let clickOutsideModal = false;
+</script>
+
+<Button on:click={() => (clickOutsideModal = true)}>Get Started</Button>
+
+<Modal title="Questionnaire" bind:open={clickOutsideModal} autoclose outsideclose>
+    <h1>Are you using a mobile device?</h1>
+    <ButtonGroup>
+        <Button>Yes</Button>
+        <Button>No</Button>
+    </ButtonGroup>
+</Modal>
